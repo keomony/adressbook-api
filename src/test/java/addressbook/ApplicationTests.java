2c1778d8e1a614e438e8af597e5812d01d603549
Keomony <kkhun@thoughtworks.com>
2019-10-19 23:50:50 +0100

    Use WeTestClient for rest template in intergration test

--- src/test/java/addressbook/ApplicationTests.java
+++ src/test/java/addressbook/ApplicationTests.java
@@ -1,16 +1,41 @@
 package addressbook;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    private WebTestClient webClient;
+
+    private String json = "{\"id\":\"1\",\"surname\":\"Chea\",\"firstName\":\"Makera\",\"postcode\":\"JK21 4PG\"}";
+
     @Test
     public void contextLoads() {
+    }
+
+    @Before
+    public void setUp(){
+
+        webClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:8080")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .build();
+    }
+
+    @Test
+    public void shouldBeAbleToAddANewCustomer() {
+
+        webClient.post().uri("/customers/")
+                .syncBody(json)
+                .exchange().expectStatus().isOk();
+
     }
 
 }
